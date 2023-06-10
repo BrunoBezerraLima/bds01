f6f87a52eb531856b5f907894401f756e640f8e9
BrunoBezerraLima <brunobezerralima@gmail.com>
2023-06-10 16:25:51 -0300

    Configurações do banco H2 e script sql.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Carlos', '11122233344', 1000.00, TIMESTAMP WITH TIME ZONE '1988-05-30T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Daniel', '55522233344', 1500.00, TIMESTAMP WITH TIME ZONE '1989-05-30T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Eduardo', '66622233344', 2000.00, TIMESTAMP WITH TIME ZONE '1994-05-30T10:00:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jefferson', '77722233344', 2500.00, TIMESTAMP WITH TIME ZONE '1988-05-30T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Felipe', '55522233344', 3000.00, TIMESTAMP WITH TIME ZONE '1989-05-30T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Diego', '66622233344', 3500.00, TIMESTAMP WITH TIME ZONE '1989-05-30T10:00:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Rogeio', '77722233344', 4000.00, TIMESTAMP WITH TIME ZONE '1987-05-30T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Rafael', '88822233344', 4500.00, TIMESTAMP WITH TIME ZONE '1990-05-30T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Tiago', '99922233344', 5000.00, TIMESTAMP WITH TIME ZONE '1991-05-30T10:00:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Leandro', '11155533344', 5500.00, TIMESTAMP WITH TIME ZONE '1996-05-30T10:00:00Z', 1);
